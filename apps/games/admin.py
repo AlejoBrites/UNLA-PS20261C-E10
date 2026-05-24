a7ee804c3dc608198c92c4ef9a7b82e547b9fe5d
Marco Carreira <marco.carreira.22n@gmail.com>
2026-05-23 23:34:46 -0300

    feat(games): agregar funcionabilidad para agregar juegos al catalogo desde el panel de admin

--- apps/games/admin.py
+++ apps/games/admin.py
@@ -1,3 +1,9 @@
 # TODO Fase 2 — Integrante 2: registrar Category y Game en el admin
 from django.contrib import admin
 from .models import Category, Game
+
+# Agregamos categoria en el panel admin
+admin.site.register(Category)
+
+# Agregamos juegos en el panel de admin
+admin.site.register(Game)
